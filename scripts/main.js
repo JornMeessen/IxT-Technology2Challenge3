aed5b25c5b45feaac2de6bf7b075cae524c79463
JornMeessen <jornmeessen@hotmail.com>
2020-04-15 13:16:30 +0200

    Update codefiles

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ map.addControl(new mapboxgl.NavigationControl());
 map.on('load', function () {
 
   // nieuwe source places
-    map.addSource('places', {
+  map.addSource('places', {
     'type': 'geojson',
     'data': {
       'type': 'FeatureCollection',
@@ -28,56 +28,52 @@ map.on('load', function () {
     }
   });
 
-buildLocationList(restaurants);
+  buildLocationList(restaurants);
   addMarkers();
 });
 
 function openweathermap(currentFeature) {
-    // Usually you do not want to call an api multiple times, but in this case we have to
-    // because the openWeatherMap API does not allow multiple lat lon coords in one request.
+  
+  // Krijg het actuele weer op basis van de coördinaten van de restauranten
+  var request = openWeatherMapUrl + '?' + 'appid=' + openWeatherMapUrlApiKey + '&lon=' + currentFeature.coordinates[0] + '&lat=' + currentFeature.coordinates[1];
 
-    var request = openWeatherMapUrl + '?' + 'appid=' + openWeatherMapUrlApiKey + '&lon=' + currentFeature.coordinates[0] + '&lat=' + currentFeature.coordinates[1];
-
-    // Get current weather based on cities' coordinates
-    fetch(request)
-      .then(function(response) {
-        if(!response.ok) throw Error(response.statusText);
-        return response.json();
-      })
-      .then(function(response) {
+  fetch(request)
+  .then(function(response) {
+    if(!response.ok) throw Error(response.statusText);
+    return response.json();
+  })
+  .then(function(response) {
         
-        // variable voor weather article 
-        var today = new Date();
-        var dagen = new Array('Sundag', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-        var maanden = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+    // variabel voor weerartikel 
+    var today = new Date();
+    var dagen = new Array('Sundag', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    var maanden = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
 
-        var city = currentFeature.properties.city;
-        var type = response.weather[0].description;
-        var iconUrl = 'http://openweathermap.org/img/w/'+response.weather[0].icon+'.png';
-        var degC = Math.floor(response.main.temp - 273.15);
+    var city = currentFeature.properties.city;
+    var type = response.weather[0].description;
+    var iconUrl = 'http://openweathermap.org/img/w/'+response.weather[0].icon+'.png';
+    var degC = Math.floor(response.main.temp - 273.15);
 
-        // render weather in weather article,
-        var weatherBox = document.getElementById('weather');
-        //weatherBox.innerHTML = +  ' ' + type + '<br>' + '<img src="'+iconUrl+'"> </div>';
-      
-        forecastMessage =    '<div class="forecastMoment">';
-        forecastMessage +=   '<h3> What is the weather in:</h3>';
-        forecastMessage +=   '<h4>'+ city + ' ' + degC + '&#176;C </h4>'; 
-        forecastMessage +=   '<h4>' + dagen[today.getDay()] + ' ' + today.getDate() + ' ' + maanden[today.getMonth()] + '</h4>'; 
-        forecastMessage +=   '<h4>'+ type +'</h4>';
-        forecastMessage +=   '<div><img src="'+iconUrl+'"></div>';
-        forecastMessage +=   '</div>';
+    // render weerartikel
+    var weatherBox = document.getElementById('weather');
+    
+    // innerHTML variablen van weerartikel    
+    forecastMessage =    '<div class="forecastMoment">';
+    forecastMessage +=   '<h3> What is the weather in:</h3>';
+    forecastMessage +=   '<h4>'+ city + ' ' + degC + '&#176;C </h4>'; 
+    forecastMessage +=   '<h4>' + dagen[today.getDay()] + ' ' + today.getDate() + ' ' + maanden[today.getMonth()] + '</h4>'; 
+    forecastMessage +=   '<h4>'+ type +'</h4>';
+    forecastMessage +=   '<div><img src="'+iconUrl+'"></div>';
+    forecastMessage +=   '</div>';
 
-        weatherBox.innerHTML = forecastMessage;
-      })
+    weatherBox.innerHTML = forecastMessage;
+  })
 
+  .catch(function (error) {
+    console.log('ERROR:', error);
+  });
 
-
-      .catch(function (error) {
-        console.log('ERROR:', error);
-      });
-
-  };
+};
 
 // Voeg een markering toe aan de kaart voor elke winkelvermelding.
 function addMarkers() {
@@ -113,9 +109,6 @@ function addMarkers() {
     
       // Sluit alle andere pop-ups en geef een pop-up weer voor de aangeklikte winkel
       createPopUp(marker);
-
-      // de weerbox actief maken.
-      //document.getElementById("weather").classList.add('weatherVisible');
     
       // Markeer vermelding in zijbalk
       var activeItem = document.getElementsByClassName('active');
@@ -165,40 +158,36 @@ function buildLocationList(data) {
     var details = listing.appendChild(document.createElement('div'));
         details.innerHTML = prop.city;
         details.innerHTML += ' &middot; ' + prop.address;
-      // if (prop.phone) {
-      //   details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-      // }
 
-     /**
-           * Listen to the element and when it is clicked, do four things:
-           * 1. Update the `currentFeature` to the store associated with the clicked link
-           * 2. Fly to the point
-           * 3. Close all other popups and display popup for clicked store
-           * 4. Highlight listing in sidebar (and remove highlight for all other listings)
-           **/
+    /*  Luister naar het element en doe vier dingen wanneer erop wordt geklikt
+      1. Werk de `currentFeature` bij naar de winkel die is gekoppeld aan de aangeklikte link
+      2. Vlieg naar het punt
+      3. Sluit alle andere pop-ups en geef een pop-up weer voor de aangeklikte winkel
+      4. Markeer vermelding in zijbalk (en verwijder markering voor alle andere vermeldingen)
+    */
 
-          link.addEventListener('click', function(e) {
-            for (var i = 0; i < restaurants.length; i++) {
-              if (this.id === 'link-' + restaurants[i].properties.id) {
-                var clickedListing = restaurants[i];
-                flyToStore(clickedListing);
-                createPopUp(clickedListing);
-                openweathermap(clickedListing);
-                document.getElementById("weather").classList.add('weatherVisible');
-              }
-            }
-            var activeItem = document.getElementsByClassName('active');
-            if (activeItem[0]) {
-              activeItem[0].classList.remove('active');
-            }
-            this.parentNode.classList.add('active');
-          });
-      });
-  }
+    link.addEventListener('click', function(e) {
+      for (var i = 0; i < restaurants.length; i++) {
+        if (this.id === 'link-' + restaurants[i].properties.id) {
+          var clickedListing = restaurants[i];
+          flyToStore(clickedListing);
+          createPopUp(clickedListing);
+          openweathermap(clickedListing);
+          document.getElementById("weather").classList.add('weatherVisible');
+        }
+      }
+      
+      var activeItem = document.getElementsByClassName('active');
+      if (activeItem[0]) {
+        activeItem[0].classList.remove('active');
+      }
+      this.parentNode.classList.add('active');
+    });
+  });
+}
 
-/*  Gebruik 'flyTo' van Mapbox GL JS om de camera soepel te verplaatsen
-    een bepaald middelpunt.
-*/
+//  Gebruik 'flyTo' van Mapbox GL JS om de camera soepel te verplaatsen een bepaald middelpunt.
+
 function flyToStore(currentFeature) {
   map.flyTo({
     center: currentFeature.coordinates,
@@ -209,7 +198,9 @@ function flyToStore(currentFeature) {
 // Maak een Mapbox GL JS `Popup`.
 function createPopUp(currentFeature) {
   var popUps = document.getElementsByClassName('mapboxgl-popup');
+  
   if (popUps[0]) popUps[0].remove();
+  
   var popup = new mapboxgl.Popup({ closeOnClick: true })
   .setLngLat(currentFeature.coordinates)
   .setHTML(
@@ -222,4 +213,3 @@ function createPopUp(currentFeature) {
   )
   .addTo(map);
 }
-
